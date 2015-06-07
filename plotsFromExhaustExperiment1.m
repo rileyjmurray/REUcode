b2daf19a2f340c4e961698a8c907683a34bd70f2
Riley John Murray <RJMurray@Rileys-MacBook-Pro.local>
2015-06-07 16:38:11 -0400

    report on experiments

--- plotsFromExhaustExperiment1.m
+++ plotsFromExhaustExperiment1.m
@@ -3,11 +3,9 @@ load('exhaust_experiment1.mat');
 
 %% All
 
-figure
-
 % plot Number of DataCenters (X) and Number of Servers / DataCenter (Y)
 %   against FailureRate
-subplot(1,2,1);
+figure
 stem3(Inputs(:,3),Inputs(:,4),Outputs(:,1));
 xlabel('Number of DataCenters')
 ylabel('Number of Servers / DataCenter');
@@ -15,7 +13,7 @@ zlabel('FailureRate');
 
 % plot Number of DataCenters (X) and Number of Servers / DataCenter (Y)
 %   against Performance Ratio (Modified-Monaldo / AllPerms)
-subplot(1,2,2);
+figure
 stem3(Inputs(:,3),Inputs(:,4),Outputs(:,2));
 zlim([0.9,1.1]);
 xlabel('Number of DataCenters')
