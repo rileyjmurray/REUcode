63e12a45ca876ed382e18105a1dfd37579b4e6f9
Riley John Murray <RJMurray@Rileys-MacBook-Pro.local>
2015-06-10 14:01:01 -0400

    add compare three hueristics script

--- analyzeCompareThreeHueristics.m
+++ analyzeCompareThreeHueristics.m
@@ -5,17 +5,17 @@ load('experiments/c3h1.mat');
 figure
 
 subplot(3,1,1);
-hist(Outputs(1,:) ./ Outputs(2,:));
+hist(Outputs(1,:) ./ Outputs(2,:),500);
 title('Mod / Pre');
 mean(Outputs(1,:) ./ Outputs(2,:))
 
 subplot(3,1,2);
-hist(Outputs(1,:) ./ Outputs(3,:));
+hist(Outputs(1,:) ./ Outputs(3,:),500);
 title('Mod / Awk');
 mean(Outputs(1,:) ./ Outputs(3,:))
 
 subplot(3,1,3);
-hist(Outputs(2,:) ./ Outputs(3,:));
+hist(Outputs(2,:) ./ Outputs(3,:),500);
 title('Pre / Awk');
 mean(Outputs(2,:) ./ Outputs(3,:))
 
