77ec03ab6d8eb8d0ee2287505fe5a4a2609ae746
Megan Chao <megchao@mit.edu>
2015-06-12 15:22:53 -0400

    added some comments

--- experiments/PreProcCompare2.m
+++ experiments/PreProcCompare2.m
@@ -1,30 +1,34 @@
 %%
-load('PreProcData.mat');
+load('PreProcData.mat'); % some preprocessing data (WEIGHTED)
 sameSoln = 1;
 threshold = 0.005;
+% entries of results matrix come in form makespan/sum
+% results where makespan does worse
 makespanWorse = (results(:) > sameSoln+threshold);
+
+% results where sum does worse
 sumWorse = (results(:) < sameSoln-threshold);
 
-%P1 = P(:,:,makespanWorse);
-%K1 = K(makespanWorse,:);
-%P2 = P(:,:,sumWorse);
-%K2 = K(sumWorse,:);
+%% COMPARING HISTOGRAMS
+% red = makespan does worse
+% blue = sum does worse
 
-%%
 figure
 subplot(1,3,1);
-h = histogram(results(makespanWorse), 100);
+h = histogram(results(makespanWorse), 75);
 h.FaceColor = 'r';
 title('makespan does worse');
 
 subplot(1,3,2);
 hold on
+
+% take reciprocal of results where sum does worse
 results2 = ones(size(results(sumWorse))) ./ results(sumWorse);
-histogram(results2, 100);
-h = histogram(results(makespanWorse), 100);
+histogram(results2, 75);
+h = histogram(results(makespanWorse), 75);
 h.FaceColor = 'r';
 title('both');
 
 subplot(1,3,3);
-histogram(results2, 100);
+histogram(results2, 75);
 title('sum does worse');
